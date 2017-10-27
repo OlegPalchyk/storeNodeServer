ba5688affb32c9fe957f551e330fad965c386ef6
olegpalchyk <Oleg80675958>
2017-10-27 11:33:58 +0300

    add pass generator

--- app/index.js
+++ app/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import path from'path';
 
+//routes
+import passwords from './routes/passwords';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../../', 'client/build')));
+
 // Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send(JSON.stringify({message : 'response from server'}));
-});
+app.use("/api", passwords);
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
