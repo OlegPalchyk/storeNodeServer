20369a46f8d86a253bce811952405ce8e24d4a01
Oleg palchyk <olegpalchyk@gmail.com>
2017-10-28 12:26:07 +0300

    sm fix

--- app/herokuSleep.js
+++ app/herokuSleep.js
@@ -1,6 +1,6 @@
 import http from  "http";
 
-let antiSleep = setInterval(function() {
+let antiSleep = ()=>setInterval(function() {
     http.get("http://olegpalchyk.herokuapp.com");
 }, 300000);
 export default antiSleep;
